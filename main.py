50fd01c8eb9b88b1bb4280b01070b9991772794e
tejashyamehta <tejashya@gmail.com>
2025-03-13 12:21:38 +0530

    Search method added

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def save():
     new_data = {
         website: {
             "email": email,
-            "password":password
+            "password": password
         }
     }
 
@@ -36,10 +36,8 @@ def save():
 
     else:
         try:
-            with open("password_data.json", "w") as data_file:  # "a" stands for append mode
-
+            with open("password_data.json", "r") as data_file:  # "a" stands for append mode
                 data = json.load(data_file)
-                data.update(new_data)
 
         except FileNotFoundError:
             with open("password_data.json", "w") as data_file:
@@ -54,13 +52,27 @@ def save():
                 #Saving updated data
                 json.dump(data, data_file, indent=4)
 
-                website_entry.delete(0,END)
-
         finally:
-            email_entry.delete(0,END)
+            website_entry.delete(0, END)
             password_entry.delete(0,END)
 
+# ---------------------------- Finding Password ------------------------------- #
+def find_password():
+    website = website_entry.get()
 
+    try:
+        with open("password_data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No Data File Found.")
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details of {website} exists.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -79,20 +91,23 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-website_entry = Entry(width=50)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=30)
+website_entry.grid(row=1, column=1)
 
-email_entry = Entry(width=50)
-email_entry.grid(row=2, column=1, columnspan=2)
+search_button = Button(text="Search", width=20, command=find_password)
+search_button.grid(row=1, column=2, pady=5)
+
+email_entry = Entry(width=55)
+email_entry.grid(row=2, column=1, columnspan=2, sticky="w", padx=10)
 email_entry.insert(0, "xyz@gmail.com")
 
-password_entry = Entry(width=21)
+password_entry = Entry(width=30)
 password_entry.grid(row=3, column=1)
 
 #Buttons
 generate_password_button = Button(text="Generate Password", width=20, command=generate_password)
-generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=35, command=save)
-add_button.grid(row=4, column=1)
+generate_password_button.grid(row=3, column=2, pady=5)
+add_button = Button(text="Add", width=30, command=save)
+add_button.grid(row=4, column=1, columnspan=2, sticky="w", padx=5)
 
 window.mainloop()
